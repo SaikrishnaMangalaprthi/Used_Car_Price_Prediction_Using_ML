29c98c109274f32e8faeb2e59119aa4abe311d8d
SaikrishnaMangalaprthi <msaikrishna502@gmail.com>
2026-06-09 20:38:10 +0530

    changes

--- vehicle_value_prediction/settings.py
+++ vehicle_value_prediction/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-local-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = [
+    'auto-worth-ai.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
